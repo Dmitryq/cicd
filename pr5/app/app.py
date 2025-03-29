23a5abe2df1c70601e9a67b8c9c091e7fea172ff
Dmitryq <thepogorny@gmail.com>
2025-03-29 17:04:56 +0300

    Тестим 5пр

---
+++ pr5/app/app.py
@@ -0,0 +1,18 @@
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+redis_hits = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+  try:
+    return redis_hits.incr('hits')
+  except redis.exception.ConnectionError as e:
+    raise e
+
+@app.route('/')
+def hello():
+  count = get_hit_count()
+  return f"Количество загрузок страницы: {count}.\n"
+
